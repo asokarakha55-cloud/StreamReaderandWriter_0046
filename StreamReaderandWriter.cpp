8b42629de9a27e63588f919b1670b6e90aea5f21
asokarakha55-cloud <asokarakha55@gmail.com>
2026-06-01 10:13:21 +0700

    Menambahkan perulangan setiap baris

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -40,5 +40,10 @@ int main()
     // kondisi jika file ada
     if (infile.is_open())
     {
+        // perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
     }
 }
